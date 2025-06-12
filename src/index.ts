6b2321a7d81e771a34d80233599add389fb6bd8a
z1319 <zhangcg@dtdream.com>
2025-06-12 15:31:00 +0800

    feat: vue脚手架

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { version } from '../package.json';
+import { create } from './command/create';
+import { update } from './command/update';
+const program = new Command('heiqi');
+program.version(version, '-v --version');
+
+program
+  .command('update')
+  .description('更新脚手架 dawei-cli')
+  .action(async () => {
+    await update();
+  });
+
+program
+  .command('create')
+  .description('创建一个新项目')
+  .argument('[name]', '项目名称')
+  .action(async (dirName) => {
+    create(dirName);
+    // if (dirName) {
+    //     create(dirName);
+    // } else {
+    //     console.log(`create ${dirName}`)
+    // }
+  });
+
+program.parse();
